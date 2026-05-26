9967c1432cb9f1978c3cb1d972e9cff1ddc5ed1d
JauniKapauni <riches-goods.54@icloud.com>
2026-05-26 14:24:31 +0200

    fix: replace static values with dynamic

--- src/main/java/de/jaunikapauni/axchat/manager/ChatManager.java
+++ src/main/java/de/jaunikapauni/axchat/manager/ChatManager.java
@@ -1,11 +1,17 @@
 package de.jaunikapauni.axchat.manager;
 
+import de.jaunikapauni.axchat.AxChat;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
 public class ChatManager {
+    AxChat reference;
+    public ChatManager(AxChat reference){
+        this.reference = reference;
+    }
+
     Jedis publisher;
     public ChatManager(String host, int port){
         publisher = new Jedis(host, port);
@@ -18,7 +24,7 @@ public class ChatManager {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try(Jedis subscriber = new Jedis("localhost", 6379)){
+                try(Jedis subscriber = new Jedis(reference.getConfig().getString("redis.host"), reference.getConfig().getInt("redis.port"))){
                     subscriber.subscribe(new JedisPubSub() {
                         @Override
                         public void onMessage(String channel, String message) {
