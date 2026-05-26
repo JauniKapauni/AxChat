41702bd6990312d39c8a7b7dcafc83b5c55e4764
JauniKapauni <riches-goods.54@icloud.com>
2026-05-26 14:14:46 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axchat/AxChat.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
