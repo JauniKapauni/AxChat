a74225f186275919cf36ec43009a14826f64581d
JauniKapauni <riches-goods.54@icloud.com>
2026-06-11 09:16:05 +0200

    fix: add perm check

--- src/main/java/de/jaunikapauni/axchat/command/ReloadCommand.java
+++ src/main/java/de/jaunikapauni/axchat/command/ReloadCommand.java
@@ -4,6 +4,7 @@ import de.jaunikapauni.axchat.AxChat;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand implements CommandExecutor {
@@ -13,8 +14,13 @@ public class ReloadCommand implements CommandExecutor {
     }
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        Player p = (Player) sender;
+        if(!p.hasPermission("axchat.msg")){
+            p.sendMessage("You don't have the permission! [axchat.reload]");
+            return true;
+        }
         reference.reloadConfig();
-        sender.sendMessage("config.yml was reloaded!");
+        p.sendMessage("config.yml was reloaded!");
         return true;
     }
 }
