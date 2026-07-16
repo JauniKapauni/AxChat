aa1c4352cada84053fb972dd1e9233736f209e44
JauniKapauni <riches-goods.54@icloud.com>
2026-07-16 12:54:27 +0200

    fix(redis): prevent exception

--- src/main/java/de/jaunikapauni/axchat/manager/ChatManager.java
+++ src/main/java/de/jaunikapauni/axchat/manager/ChatManager.java
@@ -67,10 +67,10 @@ public class ChatManager {
                     subscriber.subscribe(new JedisPubSub() {
                         @Override
                         public void onMessage(String channel, String message) {
-                            if(message.length() < 3){
+                            String[] messageParts = message.split(";", 3);
+                            if(messageParts.length < 3){
                                 return;
                             }
-                            String[] messageParts = message.split(";", 3);
                             String sourcePlayer = messageParts[0];
                             String targetName = messageParts[1];
                             String msg = messageParts[2];
