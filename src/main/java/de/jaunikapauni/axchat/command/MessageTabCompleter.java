bf56756da02bc769c58c64ab5fca0b4cb9997ee4
JauniKapauni <riches-goods.54@icloud.com>
2026-07-16 13:20:32 +0200

    fix(tabcompleter): cannot be async

--- src/main/java/de/jaunikapauni/axchat/command/MessageTabCompleter.java
+++ src/main/java/de/jaunikapauni/axchat/command/MessageTabCompleter.java
@@ -13,21 +13,21 @@ import java.util.List;
 
 public class MessageTabCompleter implements TabCompleter {
     AxChat reference;
-    public MessageTabCompleter(AxChat reference){
+
+    public MessageTabCompleter(AxChat reference) {
         this.reference = reference;
     }
+
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         List<String> list = new ArrayList<>();
-        if(args.length == 1){
+        if (args.length == 1) {
             String input = args[0].toLowerCase();
-            Bukkit.getScheduler().runTaskAsynchronously(reference, () -> {
-                for(String name : reference.getPlayerManager().getOnlinePlayers()){
-                    if(name.toLowerCase().startsWith(input)){
-                        list.add(name);
-                    }
+            for (String name : reference.getPlayerManager().getOnlinePlayers()) {
+                if (name.toLowerCase().startsWith(input)) {
+                    list.add(name);
                 }
-            });
+            }
         }
         return list;
     }
